ccf43877192edc9dafb87a2624b0b4c404759f8e
asakuchi <49332371+asakuchi@users.noreply.github.com>
2024-03-03 14:32:50 +0900

    n固定版

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,11 @@
 use wasm_bindgen::prelude::*;
 
+mod tools;
+mod vis;
+
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
-    "".to_string()
+    tools::gen(seed as u64).to_string()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -13,15 +16,49 @@ pub struct Ret {
 }
 
 #[wasm_bindgen]
-pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
-    Ret {
-        score: 0,
-        err: "".to_string(),
-        svg: "".to_string(),
+pub fn vis(input: String, output: String, turn: usize) -> Ret {
+    let input = tools::parse_input(&input);
+
+    // turn を考慮
+    let out = tools::parse_output(&input, &output, turn);
+
+    let (score, err, svg) = match out {
+        Ok(out) => {
+            let (score, err) = tools::compute_score(&input, &out);
+
+            if err.len() > 0 {
+                (0, err, "".to_string())
+            } else {
+                let (_score, err, svg) = vis::vis(&input, &out, turn);
+
+                if err.len() > 0 {
+                    (0, err, "".to_string())
+                } else {
+                    (score, err, svg)
+                }
+            }
+        }
+        Err(err) => (0, err, "".to_string()),
+    };
+
+    if err.len() > 0 {
+        return Ret {
+            score: 0,
+            err,
+            svg: "".to_string(),
+        };
     }
+
+    Ret { score, err, svg }
 }
 
 #[wasm_bindgen]
-pub fn get_max_turn(_input: String, _output: String) -> usize {
-    0
+pub fn get_max_turn(input: String, output: String) -> usize {
+    let input = tools::parse_input(&input);
+    let out = tools::parse_output(&input, &output, usize::MAX);
+
+    match out {
+        Ok(out) => out.out.len(),
+        Err(_err) => 0,
+    }
 }
---
+++ wasm/src/vis.rs
@@ -0,0 +1,114 @@
+#![allow(non_snake_case, unused_macros)]
+// use itertools::Itertools;
+// use proconio::input;
+// use rand::prelude::*;
+// use std::{collections::VecDeque, usize};
+use svg::node::{
+    element::{Rectangle, Style},
+    Text,
+};
+use web_sys::console::log_1;
+
+use super::tools;
+
+const VIEW_SIZE: usize = 100;
+
+pub fn vis(input: &tools::Input, output: &tools::Output, _turn: usize) -> (i64, String, String) {
+    log_1(&"start vis".into());
+
+    // scoreはこの関数では計算しない
+    let score = 999999999;
+
+    let mut a = input.a.clone();
+    let mut p1 = (output.start.0, output.start.1);
+    let mut p2 = (output.start.2, output.start.3);
+
+    for &(do_swap, dir1, dir2) in &output.out {
+        if do_swap {
+            let tmp = a[p1.0][p1.1];
+            a[p1.0][p1.1] = a[p2.0][p2.1];
+            a[p2.0][p2.1] = tmp;
+        }
+        if dir1 != !0 {
+            if !tools::can_move(input.n, &input.hs, &input.vs, p1.0, p1.1, dir1) {
+                return (
+                    0,
+                    format!("Invalid move: {}", tools::DIRS[dir1]),
+                    // (a, p1, p2),
+                    "".to_string(),
+                );
+            }
+            p1.0 += tools::DIJ[dir1].0;
+            p1.1 += tools::DIJ[dir1].1;
+        }
+        if dir2 != !0 {
+            if !tools::can_move(input.n, &input.hs, &input.vs, p2.0, p2.1, dir2) {
+                return (
+                    0,
+                    format!("Invalid move: {}", tools::DIRS[dir2]),
+                    // (a, p1, p2),
+                    "".to_string(),
+                );
+            }
+            p2.0 += tools::DIJ[dir2].0;
+            p2.1 += tools::DIJ[dir2].1;
+        }
+    }
+
+    let mut doc = svg::Document::new()
+        .set("id", "vis")
+        .set("viewBox", (0, 0, VIEW_SIZE, VIEW_SIZE))
+        .set("width", 800)
+        .set("height", 800)
+        .set("style", "background-color:white; border: solid");
+
+    doc = doc.add(Style::new(format!(
+        "text {{text-anchor: middle;dominant-baseline: central; font-size: {}}}",
+        6
+    )));
+
+    for y in 0..input.n {
+        for x in 0..input.n {
+            doc = doc.add(
+                rect(
+                    x * 10,
+                    y * 10,
+                    10,
+                    10,
+                    if (y, x) == p1 {
+                        "blue"
+                    } else if (y, x) == p2 {
+                        "green"
+                    } else {
+                        "white"
+                    },
+                )
+                .set("stroke", "black")
+                .set("stroke-width", 1)
+                .set("class", "box"),
+            );
+
+            let text_node = svg::node::Text::new(a[y][x].to_string());
+            let text_element = svg::node::element::Text::new()
+                .set("x", x * 10 + 5)
+                .set("y", y * 10 + 5)
+                .set("fill", "cornflowerblue")
+                .set("font-family", "游明朝")
+                .set("font-size", "3")
+                .add(text_node);
+
+            doc = doc.add(text_element);
+        }
+    }
+
+    (score as i64, "".to_string(), doc.to_string())
+}
+
+pub fn rect(x: usize, y: usize, w: usize, h: usize, fill: &str) -> Rectangle {
+    Rectangle::new()
+        .set("x", x)
+        .set("y", y)
+        .set("width", w)
+        .set("height", h)
+        .set("fill", fill)
+}
